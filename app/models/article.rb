15de8274720726a82a0e16995af6f9a9cdd368e0
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-04 13:26:16 -0400

    add comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :comments #other side of relationship between articles and comments
+
   validates :title, presence: true #must contain at least one non-whitespace char
   validates :body, presence: true, length: { minimum: 10 }
 end
