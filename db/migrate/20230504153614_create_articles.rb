bdc79910446b84db117f36bd8809d62be2a1e18a
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-04 12:02:19 -0400

    create one article, save to db

--- db/migrate/20230504153614_create_articles.rb
+++ db/migrate/20230504153614_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-      t.string :title
+      t.string :title 
       t.text :body
 
-      t.timestamps
+      t.timestamps #defines created_at, updated_at cols
     end
   end
 end
