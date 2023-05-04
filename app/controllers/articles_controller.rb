f0bc2e591b1a57adcc7bd7a166b68098ac951f88
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-04 12:12:55 -0400

    add show action, show page, add link to show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all #index view retrieves all articles in db
   end
+
+  def show
+    @article = Article.find(params[:id]) #get the article with id
+  end
 end
